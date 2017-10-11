3dae6430041c3aaa389e7a7ad13f6e470f38f173
vava24680 <9203700@gmail.com>
2017-10-12 01:51:08 +0800

    Fix error

--- hw2/sahw2_1.sh
+++ hw2/sahw2_1.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-ls -A -R -l | awk '{if(NF>=9){printf("%s\n",$0)}}' | sort -k 5,5 -nr | awk 'BEGIN {i=0;n_dir=0;n_file=0;n_size=0;} {if($1~/^-.*/) {n_file++;n_size+=$5; if(i<5){printf("%s\n",$0);}} else {n_dir++;}} END {printf("Dir num: %d\n",n_dir);printf("File num: %d\n",n_file);printf("Total: %d\n",n_size);}'
+ls -A -R -l | awk '{if(NF>=9){printf("%s\n",$0)}}' | sort -k 5,5 -nr | awk 'BEGIN {i=0;n_dir=0;n_file=0;n_size=0;} {if($1~/^-.*/) {n_file++;n_size+=$5; if(i<5){printf("%d:%s %s\n",i+1,$5,$9);i++}} else {n_dir++;}} END {printf("Dir num: %d\n",n_dir);printf("File num: %d\n",n_file);printf("Total: %d\n",n_size);}'
