faa6bb1f113e725a88e95b16a88c353e0a2a4114
vava24680 <9203700@gmail.com>
2017-10-12 01:52:59 +0800

    Change to Eng menu

--- hw2/sahw2_2.sh
+++ hw2/sahw2_2.sh
@@ -2,8 +2,8 @@
 touch cookie-jar.txt
 status=0
 while [ $status -eq 0 ] ; do
-	account=`dialog --output-fd 1 --inputbox "請輸入學號" 20 20`
-	password=`dialog --output-fd 1 --insecure --passwordbox "請輸入單一登入系統der密碼" 20 20`
+	account=`dialog --output-fd 1 --inputbox "Please input the Account" 20 20`
+	password=`dialog --output-fd 1 --insecure --passwordbox "Please input the Portal Password" 20 20`
 	curl -s -c cookie-jar.txt https://portal.nctu.edu.tw/captcha/pic.php
 	curl -s -b cookie-jar.txt -c cookie-jar.txt https://portal.nctu.edu.tw/captcha/pitctest/pic.php -o captcha.png
 	captcha_num=`curl -s -F 'image=@./captcha.png' -i https://nasa.cs.nctu.edu.tw/sap/2017/hw2/captcha-solver/api/ | awk 'NR==8 {printf("%d",$1)}'`
@@ -23,4 +23,3 @@ curl -b cookie-jar.txt https://course.nctu.edu.tw/adSchedule.asp -o - | iconv -f
 	/<font.+><h2>&nbsp/{printf(". ");}\
 	/^[^<]/{printf("%s ",$0)}\
 	END{printf("\n");}' | column -t
-#echo ${schedule}
